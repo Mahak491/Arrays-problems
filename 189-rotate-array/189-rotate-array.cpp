3d9dff751546a351ee29f356d634fb70a4d5a1c6
Mahak shree <82505816+Mahak491@users.noreply.github.com>
2022-06-19 12:04:08 +0530

    Time: 60 ms (15.96%), Space: 25 MB (53.31%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
+    void reverse(vector<int> &num,int low,int high){
+        while(low<high){
+            swap(num[low],num[high]);
+            low++;
+            high--;
+        }
+    }
     void rotate(vector<int>& nums, int k) {
+        // brute force
      int n = nums.size();
-     if ((n == 0) || (k <= 0))
+    /* if ((n == 0) || (k <= 0))
       {
             return;
       }
@@ -18,6 +26,15 @@ public:
       for (int i = 0; i < n; i++)
       {
          nums[(i + k)%n] = numsCopy[i];
-      }   
+      } 
+       */ 
+        
+        
+        //optimal
+        k = k % n;
+        reverse(nums,0,n-k-1);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-1);
+        
     }
 };
