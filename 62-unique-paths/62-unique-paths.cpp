b29171e815f7565f387fc1dad8421af30c4b5818
Mahak shree <82505816+Mahak491@users.noreply.github.com>
2022-07-09 14:04:46 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.59%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int countWaysUtil(int i, int j, vector<vector<int> > &dp) {
+   /* int countWaysUtil(int i, int j, vector<vector<int> > &dp) {
       if(i==0 && j == 0)
         return 1;
       if(i<0 || j<0)
@@ -12,10 +12,18 @@ public:
 
       return dp[i][j] = up+left;
 
-}
+}*/
     
     int uniquePaths(int m, int n) {
-        vector<vector<int> > dp(m,vector<int>(n,-1));
-        return countWaysUtil(m-1,n-1,dp); 
+        // vector<vector<int> > dp(m,vector<int>(n,-1));
+        // return countWaysUtil(m-1,n-1,dp); 
+        
+        vector<vector<int>> dp(m,vector<int>(n,1));
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
