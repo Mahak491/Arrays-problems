21bee02937cc47350edf789ea2f443a6f6468cb1
Mahak shree <82505816+Mahak491@users.noreply.github.com>
2022-07-09 11:28:38 +0530

    Added solution - LeetHub

--- Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
+++ Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
@@ -9,14 +9,24 @@ class Solution {
   public:
     long long int nthFibonacci(long long int n){
         // code here
-        vector<int> dp(n+1);
+       /* vector<int> dp(n+1);
         dp[0] = 0;
         dp[1] = 1;
         for(int i=2;i<=n;i++){
             dp[i] = (dp[i-1]+dp[i-2])%1000000007;
         }
         return dp[n];
+    }*/
+    
+    int prev2 = 0;
+    int prev = 1;
+    for(int i=2;i<=n;i++){
+        int curi = (prev+prev2)%1000000007;
+        prev2 = prev;
+        prev = curi;
     }
+    return prev;
+ }
 };
 
 // { Driver Code Starts.
