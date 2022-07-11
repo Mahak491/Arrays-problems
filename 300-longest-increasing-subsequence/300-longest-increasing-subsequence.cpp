0aaa66f58e76ca15240f8739d68260df89aa3585
Mahak shree <82505816+Mahak491@users.noreply.github.com>
2022-07-11 11:49:04 +0530

    Time: 998 ms (12.05%), Space: 292.2 MB (6.77%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int f(int ind,int prev_ind,vector<int> &nums, vector<vector<int>> &dp,int n){
+   /* int f(int ind,int prev_ind,vector<int> &nums, vector<vector<int>> &dp,int n){
         if(ind == n) return 0;
         if(dp[ind][prev_ind+1] != -1) return dp[ind][prev_ind+1];
         
@@ -11,11 +11,27 @@ public:
             len = max(len,1+f(ind+1,ind,nums,dp,n));
         }
         return dp[ind][prev_ind+1] = len;
-    }
+    }*/
     
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return f(0,-1,nums,dp,n);
+        // vector<vector<int>> dp(n,vector<int>(n+1,-1));
+        // return f(0,-1,nums,dp,n);
+        
+        //tabulation
+        
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        
+        for(int ind = n-1; ind >= 0; ind--){
+            for(int prev = ind-1; prev >= -1; prev--){
+                int len = 0 + dp[ind+1][prev+1];
+        
+                if(prev == -1 || nums[ind] > nums[prev]){
+                    len = max(len,1+dp[ind+1][ind+1]);
+                }
+             dp[ind][prev+1] = len;
+            }
+        }
+         return dp[0][-1+1];
     }
 };
