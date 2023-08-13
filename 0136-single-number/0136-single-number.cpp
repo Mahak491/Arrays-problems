20bdd561c14e5201b01d3e58601cc4d52b2a4c7d
Mahak Shree <82505816+Mahak491@users.noreply.github.com>
2023-08-13 19:40:44 +0530

    Time: 11 ms (95.71%), Space: 16.9 MB (64.35%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-       map<int,int> mp;
+    //    map<int,int> mp;
+    //     for(int i=0;i<nums.size();i++){
+    //       mp[nums[i]]++;
+    //     }
+    //     for(auto x:mp){
+    //         if(x.second == 1){
+    //             return x.first;
+    //         }
+    //     }
+    //     return -1;
+        int xorr = 0;
         for(int i=0;i<nums.size();i++){
-          mp[nums[i]]++;
+            xorr = xorr^nums[i];
         }
-        for(auto x:mp){
-            if(x.second == 1){
-                return x.first;
-            }
-        }
-        return -1;
+        return xorr;
     }
+        
 };
