6f54fe826c9002e12ea6dfc9f57cda1aaabc8738
Mahak Shree <82505816+Mahak491@users.noreply.github.com>
2023-08-31 23:30:41 +0530

    Time: 6 ms (27.84%), Space: 12 MB (94.09%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-    int brkpnt = -1;
-    int n = nums.size();
-
-   
-    for (int i = n - 2; i >= 0; i--) {
-        if (nums[i] < nums[i + 1]) {
-            brkpnt = i;
-            break;
-        }
+       int brkpnt = -1;
+       int n = nums.size();
+       for(int i=n-2;i>=0;i--){
+           if(nums[i] < nums[i+1]){
+               brkpnt = i;
+               break;
+           }
+       }
+        
+       if(brkpnt == -1){
+          reverse(nums.begin(),nums.end());
+       }
+       else{
+       for(int i=n-1;i>=0;i--){
+           if(nums[i] > nums[brkpnt]){
+               swap(nums[i],nums[brkpnt]);
+               break;
+           }
+       }
+       reverse(nums.begin()+brkpnt+1,nums.end());
+       }
     }
-
-    
-    if (brkpnt == -1) {
-        reverse(nums.begin(), nums.end());
-    }
-    else {
-        for (int i = n - 1; i >= 0; i--) {
-            if (nums[i] > nums[brkpnt]) {
-                swap(nums[i], nums[brkpnt]);
-                break;
-            }
-        }
-        reverse(nums.begin() + brkpnt + 1, nums.end());
-    }
-}
-
 };
